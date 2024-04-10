fe6b8477192343b799c49861032afb73860771c5
NickQZ <nic.ejones25@gmail.com>
2024-04-10 12:40:47 +1200

    After merging of Branches I had to create a new Repository as I was getting rejections on my pushes.
    Current Version
    
    V.1.5

---
+++ bit504_assignment2_PartAStudent/src/bit504_assignment2_PartAStudent/GameState.java
@@ -0,0 +1,7 @@
+package bit504_assignment2_PartAStudent;
+// Created enumeration for the variable Game State.
+// This Included; Playing, Draw, Cross_won, Nought_won
+public enum GameState {
+
+	Playing, Draw, Cross_won, Nought_won
+}
---
+++ bit504_assignment2_PartAStudent/src/bit504_assignment2_PartAStudent/Player.java
@@ -0,0 +1,8 @@
+package bit504_assignment2_PartAStudent;
+/**
+ * Enumeration for the players move
+ */
+
+public enum Player {
+	Empty, Cross, Nought
+}
